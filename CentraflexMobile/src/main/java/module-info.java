7855603a0798d4d90a9670d89f53299978a970d8
Stefan Prelle <spr@plusnet.de>
2021-05-07 17:05:42 +0200

    Mit gradle

---
+++ CentraflexMobile/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module cfxmobile {
+	exports de.plusnet.centraflex.mobile.cells;
+	exports de.plusnet.centraflex.mobile;
+
+	requires centraflex.telephony;
+	requires centraflex.telephony.xsi;
+	requires com.gluonhq.attach.settings;
+	requires de.centraflex.libxsi;
+	requires java.desktop;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.extensions;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires org.apache.logging.log4j;
+}
