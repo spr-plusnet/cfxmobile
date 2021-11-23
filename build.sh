90ca1f6c3407340da2ba9c25cc0bcd34d54dd672
Stefan Prelle <spr@plusnet.de>
2021-11-23 16:21:27 +0100

    Update GraalVM

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/opt/graalvm-ce-java11-21.1.0
-export GRAALVM_HOME=/opt/graalvm-ce-java11-21.1.0
+export JAVA_HOME=/opt/graalvm-ce-java11-21.3.0
+export GRAALVM_HOME=/opt/graalvm-ce-java11-21.3.0
 mvn clean gluonfx:build
 
